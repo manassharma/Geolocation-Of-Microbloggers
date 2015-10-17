d79723bb683eb242fe5a5b2373bbec01f1a11c2b
Manas Sharma <manas_sharma@msn.com>
2015-10-17 01:31:04 -0700

    Manas' training set collator

---
+++ trainingSetGenerator.py
@@ -0,0 +1,33 @@
+import os
+from itertools import izip
+
+userInfo = open("training_set_users.txt", "r")
+tweetInfo = open("training_set_tweets.txt", "r")
+trainingSet = open("training_combined.txt", "w")
+    
+def setGenerator():
+    for iteratorTweets in tweetInfo:
+        tweetBuffer = str(iteratorTweets)
+        myTweetArray = tweetBuffer.split("\t")
+        for iteratorUid in userInfo:
+            userBuffer = str(iteratorUid)
+            myUserArray = userBuffer.split("\t")
+
+            try:
+                if (myTweetArray[0] == myUserArray[0]):
+                    trainingSet.write(tweetBuffer+"\t"+userBuffer[1]+"\n")
+            except Exception:
+                pass
+        
+        
+    
+if __name__ == '__main__':
+
+    print "Bootstrapping training data creation script **************"
+    
+    setGenerator()
+
+    
+    
+    
+    
